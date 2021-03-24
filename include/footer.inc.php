2a335c65bcf76e6b7ea66254c7e49d3e4dc39853
Antoine <43754408+Klbgr@users.noreply.github.com>
2021-03-24 16:25:22 +0100

    Début footer

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,5 +1,7 @@
-    <footer>
-
+    <footer id="footer">
+        <p class="left">Auteurs : Maxime Grodet &amp; Antoine Qiu</p>
+        <p class="right">Version du 24/03/2021</p>
+        <p class="center"><a href="credits.php">Crédits</a></p>
     </footer>
 </body>
 </html>
