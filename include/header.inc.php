f3daf5ebcedd93afdf0a2bc12676cb603dedbb9d
e-mgrodet <grodet95@gmail.com>
2021-03-25 10:27:51 +0100

    changement arborecensce de fichier + debut header

---
+++ include/header.inc.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="Projet DevWeb"/>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <img src="ressources/logo/logo.png" alt="logo"/>
+            <ul>
+                <li><a>page 1</a></li>
+                <li><a>page 2</a></li>
+                <li><a>page 3</a></li>
+                <li><a>page 4</a></li>
+            </ul>
+        </nav>
+    </header>
--- projet devweb/site web/include/header.inc.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="style.css"/>
-</head>
-<body>
