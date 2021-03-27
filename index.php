531bcb9c01e53c0c3e04c66d3a429a392cd5cf4b
e-mgrodet <grodet95@gmail.com>
2021-03-27 17:18:41 +0100

    navbar sticky et content

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 include_once 'include/header.inc.php';
 ?>
 
-<h1>CineHub</h1>
+    <div class="card">
+        <h1>CineHub</h1>
+        <div>
+            <p>Bienvenue sur CineHub ! Le site pour les passioner de film et série en tout genre !</p>
+            <p>Sur notre site vous pourrez trouver une large sélection de films et séries que vous pouvez explorer et trier !</p>
+        </div>
+        <button>Voir les films et séries</button>
+    </div>
 
 <?php
 include_once 'include/footer.inc.php';
