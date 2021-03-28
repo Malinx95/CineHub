4fd975274e8e2486e7010e556561c40ab6e41580
Antoine <43754408+Klbgr@users.noreply.github.com>
2021-03-28 20:59:47 +0200

    index
    -5w dans les métadonnées
    -changement de div en section
    -ajout de la partie basse
    -création fichier functions.inc.php
    -ajout de la fonction nasa

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 include_once 'include/header.inc.php';
+include 'include/functions.inc.php';
 ?>
 
-    <div class="card">
+    <section class="card">
         <h1>CineHub</h1>
         <div>
             <p>Bienvenue sur CineHub ! Le site pour les passioner de film et série en tout genre !</p>
             <p>Sur notre site vous pourrez trouver une large sélection de films et séries que vous pouvez explorer et trier !</p>
         </div>
         <button>Voir les films et séries</button>
-    </div>
+    </section>
+    <section id="more">
+        <div>
+            <h2>Informations</h2>
+            <p>Présentation Présentation Présentation Présentation Présentation Présentation Présentation </p>
+            <p>Présentation Présentation Présentation Présentation Présentation Présentation Présentation </p>
+            <p>Présentation Présentation Présentation Présentation Présentation Présentation Présentation </p>
+            <p>Présentation Présentation Présentation Présentation Présentation Présentation Présentation </p>
+        </div>
+        <img src="<?php echo nasa()?>" width="500" alt="image nasa">
+
+
+    </section>
 
 <?php
 include_once 'include/footer.inc.php';
