872261d3c527698c53f0771ae144406fa71106df
Antoine <43754408+Klbgr@users.noreply.github.com>
2021-03-31 14:50:50 +0200

    suppression de class dans footer

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,7 +1,7 @@
     <footer id="footer">
-        <p class="left">Auteurs : Maxime Grodet &amp; Antoine Qiu</p>
-        <p class="center"><a href="credits.php">Crédits</a></p>
-        <p class="right">Version du 28/03/2021</p>
+        <p>Auteurs : Maxime Grodet &amp; Antoine Qiu</p>
+        <p><a href="credits.php">Crédits</a></p>
+        <p>Version du 28/03/2021</p>
         <a class="up" href="#header"><button>UP</button></a>
     </footer>
 </body>
