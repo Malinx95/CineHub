8962ef388a69d2b990784633a981a653de0987f4
maxime grodet <grodet95@gmail.com>
2021-04-27 16:53:08 +0200

    fix top

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'include/functions.inc.php';
         <h2>Introduction</h2>
         <div class="center accueil">
             <img class="biglogo" src="ressources/logo/logo.png" alt="logo cinehub"/>
-            <p>Bienvenue sur CineHub ! Le site pour les passionés de films et séries en tout genre !</p>
+            <p>Bienvenue sur CineHub ! Le site pour les passionnés de films et séries en tout genre !</p>
             <p>Cliquez sur le bouton ci-dessous pour commencer !</p>
             <a class="voir button" href="rechercher.php">Voir les films et séries</a>
         </div>
@@ -19,9 +19,9 @@ include 'include/functions.inc.php';
         <h2>Informations</h2>
         <div class="center accueil">
             <h3>Présentation</h3>
-            <p>Ce site à été crée dans le cadre de l'Unité d'Enseignement Developpement Web 2020-2021.</p>
+            <p>Ce site à été crée dans le cadre de l'Unité d'Enseignement Développement Web 2020-2021.</p>
             <p>Réalisé par Maxime Grodet &amp; Antoine Qiu en L2 Informatique, groupe C.</p>
-            <p>Sur notre site vous pourrez trouver une large sélection de films et séries que vous pouvez explorer et trier !</p>
+            <p>Sur notre site, vous pourrez trouver une large sélection de films et de séries que vous pouvez explorer et trier !</p>
             <h3>Image du jour</h3>
             <?php echo nasa()?>
             <h3>Votre Géolocalisation</h3>
